2e1949ff91497675e136e6ae53131ba69432173b
MiroslavaVikhrenko <m.vikhrenko@gmail.com>
2025-02-18 15:28:53 -0700

    Add properties to TodoItem model class

--- TodoApi/TodoItem.cs
+++ TodoApi/TodoItem.cs
@@ -3,5 +3,8 @@
     // model class
     public class TodoItem
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public bool IsComplete { get; set; }
     }
 }
