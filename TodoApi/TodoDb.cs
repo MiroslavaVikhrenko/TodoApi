9601199087f090aa9b7b563daa82e96d48173f5d
MiroslavaVikhrenko <m.vikhrenko@gmail.com>
2025-02-18 15:38:17 -0700

    Add constructor and property to TodoDb class

--- TodoApi/TodoDb.cs
+++ TodoApi/TodoDb.cs
@@ -4,5 +4,8 @@ namespace TodoApi
     // DbContext class to use EF package
     public class TodoDb : DbContext
     {
+        public TodoDb(DbContextOptions<TodoDb> options) : base(options) { }
+
+        public DbSet<TodoItem> Todos { get; set; }
     }
 }
