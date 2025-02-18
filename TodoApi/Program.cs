114e6fb99d21b5003806cc1c86de1a762c5eac9b
MiroslavaVikhrenko <m.vikhrenko@gmail.com>
2025-02-18 15:25:23 -0700

    Clean unnesessary default code and provide basic comments for structuring program.cs file

--- TodoApi/Program.cs
+++ TodoApi/Program.cs
@@ -1,6 +1,9 @@
 var builder = WebApplication.CreateBuilder(args);
+
+// Add DI - AddService() method when we are registering services into ASP.NET.
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+// After building the web app we configure ASP.NET request pipeline - Use and Map methods to configure request lifecycle.
+
 
 app.Run();
