25a1ca20bbfb26020f8f735af2aa5923c8fe2111
MiroslavaVikhrenko <m.vikhrenko@gmail.com>
2025-02-18 15:42:39 -0700

    Register DbContext object in program.cs and configure use in-memory db "TodoList"

--- TodoApi/Program.cs
+++ TodoApi/Program.cs
@@ -1,6 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+using TodoApi;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add DI - AddService() method when we are registering services into ASP.NET.
+
+builder.Services.AddDbContext<TodoDb>(opt => opt.UseInMemoryDatabase("TodoList"));
+
 var app = builder.Build();
 
 // After building the web app we configure ASP.NET request pipeline - Use and Map methods to configure request lifecycle.
